193cb0bd6c90d7f9fe2a4de023053dc1024f7095
hwaya <wingkdh@gmail.com>
2020-02-04 16:50:35 +0900

    기본 프로젝트

---
+++ Project/System/stdafx.h
@@ -0,0 +1,18 @@
+#pragma once
+// Windows 헤더 파일
+#include <windows.h>
+// C 런타임 헤더 파일입니다.
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+/* D2D */
+#pragma comment(lib, "d2d1.lib")
+#include <d2d1.h>
+#include <d2d1helper.h>
+
+#define WINSIZEX 1024
+#define WINSIZEY 768
+#define WINPOSX 100
+#define WINPOSY 100
